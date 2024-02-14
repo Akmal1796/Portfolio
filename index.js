0fe8404bc42cd80b15cee4dbad33c5c6b3d94120
Akmal1796 <mhdakmal1796@gmail.com>
2024-02-14 12:40:09 +0530

    Complete the responsive navigation bar and responsive cover page

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+const sidebar = document.querySelector('.sidebar');
+
+function showSidebar() {
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    sidebar.style.display = 'none';
+}
