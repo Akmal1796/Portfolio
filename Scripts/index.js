e90b6da01e04fc04c07c2d0da519033d1cf97128
Akmal1796 <mhdakmal1796@gmail.com>
2024-02-21 18:58:35 +0530

    I added the smooth scrolling effect to the portfolio.

--- Scripts/index.js
+++ Scripts/index.js
@@ -27,6 +27,28 @@ function hideSidebar() {
     sidebar.style.display = 'none';
 }
 
+                                                            /* Reveal Elements on Scroll */
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+
+    for(let i=0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+
                                                             /* hide/show projects */
 
 document.addEventListener('DOMContentLoaded', function () {
