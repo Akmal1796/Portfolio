40e675d30e971123676f35c76f09040664279fe7
Akmal1796 <mhdakmal1796@gmail.com>
2024-02-24 13:56:07 +0530

    I added the back-to-top button that acts like when the user scrolls down to the page and clicks this button, it brings the user to the home section smoothly.

--- Scripts/index.js
+++ Scripts/index.js
@@ -48,6 +48,61 @@ function reveal() {
     }
 }
 
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+    /**
+   * Navbar links active state on scroll
+   */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200
+      navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          navbarlink.classList.add('active')
+        } else {
+          navbarlink.classList.remove('active')
+        }
+      })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+ /**
+   * Back to top button
+   */
+    let backtotop = select('.back-to-top');
+    if (backtotop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+        } else {
+        backtotop.classList.remove('active')
+        }
+    }
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+    }
+
 
                                                             /* hide/show projects */
 
